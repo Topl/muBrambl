b54c0550b8116f96bf811772d7ccf94e42d5031c
yamir1995 <yamirtainwala@gmail.com>
2019-03-03 08:10:11 -0500

    Cleaning tests and promises strategy

--- Promises.js
+++ Promises.js
@@ -5,21 +5,37 @@ const LokiJS = require('./Requests.js')
 
 var LokiObj = new LokiJS();
 
+//Finding transaction from mempool sample usage -- not necessary 
+LokiObj.createAssets('6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', '6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', 1, 'testAssets', 0, '')
+.then(function(response) {
+  const res = JSON.parse(response);
+  console.log("--------------------------------------------------------------");
+  console.log("create assets result:");
+  console.log(response);
+  console.log(res.result.txHash);
+  LokiObj.findTransactionFromMempool(res.result.txHash)
+  .then(function(response) {
+    console.log("findTransactionFromMempool result:");
+    console.log(response);
+  });
+});
+
 //Finding confirmed transaction by using setTimeout function
 LokiObj.createAssets('6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', '6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', 1, 'testAssets', 0, '')
 .then(function(response) {
   const res = JSON.parse(response);
+  console.log("--------------------------------------------------------------");
+  console.log("create assets result:");
   console.log(response);
   console.log(res.result.txHash);
   setTimeout(function() {
     LokiObj.findTransactionById(res.result.txHash)
     .then(function(response) {
+      console.log("findTransactionById result:");
       console.log(response);
     });
   }, 10000
 );
-
-
   // const start = Date.now();
   // while(Date.now() - start > 100000) {
   //   LokiObj.findTransactionById(res.result.txHash)
--- Test.js
+++ Test.js
@@ -6,23 +6,45 @@ const LokiJS = require('./Requests.js')
 var LokiObj = new LokiJS();
 
 console.log("LokiJS prototype model tests");
-//Returns promise
-console.log(LokiObj.getBalances());
-console.log(LokiObj.createAssets('6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', '6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', 10, 'testAssets', 0, ''));
 //Using .then on promise to access json data
+// console.log('----------------------------------------------------------------');
+// console.log("getBalances result:");
+// LokiObj.getBalances()
+// .then(function(response) {
+//   console.log(response);
+// });
+
+LokiObj.createAssets('6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', '6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', 10, 'testAssets', 0, '')
+.then(function(response) {
+  console.log('----------------------------------------------------------------');
+  console.log("create 10 testAssets result:");
+  console.log(response);
+});
+
 LokiObj.transferAssets('6sYyiTguyQ455w2dGEaNbrwkAWAEYV1Zk6FtZMknWDKQ', 'A9vRt6hw7w4c7b4qEkQHYptpqBGpKM5MGoXyrkGCbrfb', 1, 'testAssets', 0, '')
 .then(function(response) {
-  //console.log(response);
+  console.log('----------------------------------------------------------------');
+  console.log("transferAssets result:");
+  console.log(response);
 });
+
 LokiObj.getBalancesByKey('A9vRt6hw7w4c7b4qEkQHYptpqBGpKM5MGoXyrkGCbrfb')
 .then(function(response) {
-  //console.log(response);
+  console.log('----------------------------------------------------------------');
+  console.log("getBalancesByKey result:");
+  console.log(response);
 });
-// LokiObj.getOpenKeyfiles()
-// .then(function(response) {
-//   console.log(response);
-// });
-// LokiObj.generateKeyfile("password")
-// .then(function(response) {
-//   console.log(response);
-// });
+
+LokiObj.getOpenKeyfiles()
+.then(function(response) {
+  console.log('----------------------------------------------------------------');
+  console.log("getOpenKeyfiles result:");
+  console.log(response);
+});
+
+LokiObj.generateKeyfile("password")
+.then(function(response) {
+  console.log('----------------------------------------------------------------');
+  console.log("generateKeyfile result:");
+  console.log(response);
+});
