fe212c161f850f68743650ab1c35ad081f750fb9
James Aman <jim.a.aman@gmail.com>
2020-03-25 20:51:53 -0500

    code cleanup

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ const Requests = require('./src/Requests.js');
 const KeyManager = require('./src/KeyManager.js');
 const Hash = require('./src/Hash.js')
 
-// this line might be wrong, I am incorporaing more into LokiJS, so changed the
-// original LokiJS require to be Requests (JAA)
-// if(typeof window !== 'undefined' && typeof window.lokijs === 'undefined') {
-//     window.LokiJS = LokiJS;
-// }
-
 module.exports = { Requests, KeyManager, Hash };
